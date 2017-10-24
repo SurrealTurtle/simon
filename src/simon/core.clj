e8ff5e43fd5064d2e3ce32b41c5148f7c6369d17
Aspasia Beneti <aspasia.beneti@gmail.com>
2017-10-24 12:28:47 +0200

    4 color squares rgb

--- src/simon/core.clj
+++ src/simon/core.clj
@@ -5,7 +5,7 @@
 
 (defn setup []
   ; Set frame rate to 30 frames per second.
-  (q/frame-rate 30)
+  #_(q/frame-rate 30)
   ; Set color mode to HSB (HSV) instead of default RGB.
   (q/color-mode :hsb)
   ; setup function returns initial state. It contains
@@ -14,17 +14,29 @@
    :angle 0})
 
 (defn update-state [state]
+  state
   ; Update sketch state by changing circle color and position.
-  {:color (mod (+ (:color state) 0.7) 255)
-   :angle (+ (:angle state) 0.4)})
+  #_{:color (mod (+ (:color state) 0.7) 255)
+   :angle (+ (:angle state) 0.1)})
 
 (defn draw-state [state]
   ; Clear the sketch by filling it with light-grey color.
   (q/background 240)
-  ; Set circle color.
-  (q/fill (:color state) 255 255)
+
+  ;; Set circle color.
+  (q/fill 255 0 0)
+  (q/rect 10 10 100 100)
+
+  (q/fill 0 255 0)
+  (q/rect 200 10 100 100)
+
+  (q/fill 0 0 255)
+  (q/rect 10 200 100 100)
+
+  (q/fill 255 255 0)
+  (q/rect 200 200 100 100)
   ; Calculate x and y coordinates of the circle.
-  (let [angle (:angle state)
+  #_(let [angle (:angle state)
         x (* 150 (q/cos angle))
         y (* 150 (q/sin angle))]
     ; Move origin point to the center of the sketch.
@@ -34,12 +46,12 @@
       (q/ellipse x y 100 100))))
 
 (q/defsketch simon
-  :title "You spin my circle right round"
+  :title "Simon says..."
   :size [500 500]
   ; setup function called only once, during sketch initialization.
   :setup setup
   ; update-state is called on each iteration before draw-state.
-  :update update-state
+  ;:update update-state
   :draw draw-state
   :features [:keep-on-top]
   ; This sketch uses functional-mode middleware.
