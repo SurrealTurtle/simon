2c97a151dd147619f2c6359f1814e66d3874e460
Aspasia Beneti <aspasia.beneti@gmail.com>
2017-10-25 18:16:25 +0200

    Logs now work

--- src/simon/core.clj
+++ src/simon/core.clj
@@ -53,7 +53,7 @@
   ; update-state is called on each iteration before draw-state.
   ;:update update-state
   :draw draw-state
-  :features [:keep-on-top]
+  :features [:keep-on-top :no-bind-output]
   ; This sketch uses functional-mode middleware.
   ; Check quil wiki for more info about middlewares and particularly
   ; fun-mode.
