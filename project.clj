e71129e55bebf466dd0c476357568b4fb09e1b56
Aspasia Beneti <aspasia.beneti@gmail.com>
2017-11-01 20:51:08 +0100

    Repl starts from core

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [quil "2.6.0"]
-                 [com.taoensso/timbre "4.10.0"]])
+                 [com.taoensso/timbre "4.10.0"]]
+
+  :repl-options {:init-ns simon.core})
