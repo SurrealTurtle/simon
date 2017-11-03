c49897dba3b5fe56151630b4ae5f2e38c6140597
Aspasia Beneti <aspasia.beneti@gmail.com>
2017-11-03 13:38:15 +0100

    WIP: when clicked changes colour. Still needs to come back to first colour and save what was clicked

--- src/simon/core.clj
+++ src/simon/core.clj
@@ -4,12 +4,17 @@
             [taoensso.timbre :as log]
             [clojure.math.combinatorics :as combo]))
 
-(defonce colors {:red [255 0 0]
-                 :green [0 255 0]
-                 :yellow [255 255 0]
-                 :blue [0 0 255]})
+(defonce basic-colors {:red [255 0 0]
+                       :green [0 255 0]
+                       :yellow [255 255 0]
+                       :blue [0 0 255]})
 
-(def edge 10)
+(defonce secondary-colors {:red [255 127 80]
+                           :green [127 255 0]
+                           :yellow [255 255 200]
+                           :blue [0 191 255]})
+
+(def edge 120)
 
 (defonce start-points {:red [10 10]
                        :green [300 300]
@@ -41,50 +46,55 @@
 
 (defn setup []
   ; Set frame rate to 30 frames per second.
-  (q/frame-rate 10)
+  (q/frame-rate 1)
+
   ; Set color mode to HSB (HSV) instead of default RGB.
   #_(q/color-mode :hsb)
   ; setup function returns initial state. It contains
   ; circle color and position.
-  {:colors colors
+  {:colors basic-colors
    :button-points {:red (populate-color-points start-points edge :red)
                    :green (populate-color-points start-points edge :green)
                    :yellow (populate-color-points start-points edge :yellow)
-                   :blue (populate-color-points start-points edge :blue)}
-   })
+                   :blue (populate-color-points start-points edge :blue)}})
 
 (defn update-state [state]
   (log/info "GOT HERE")
-  (log/spy state)
-  
+  (log/info "Updated colors " (:colors state))
+
+  state
   ; Update sketch state by changing circle color and position.
   #_{:color (mod (+ (:color state) 0.7) 255)
    :angle (+ (:angle state) 0.1)})
 
 (defn draw-state [state]
+  (log/info "DRAWWWW")
   ; Clear the sketch by filling it with light-grey color.
   (q/background 240)
 
   ;; Set circle color.
-  (apply q/fill (:red colors))
-  (q/rect 10 10 edge edge)
+  (apply q/fill (log/spy (:red (:colors state))))
+  (q/rect (first (:red start-points)) (second (:red start-points)) edge edge)
 
-  (apply q/fill (:yellow colors))
-  (q/rect 300 10 edge edge)
+  (apply q/fill (:yellow (:colors state)))
+  (q/rect (first (:yellow start-points)) (second (:yellow start-points)) edge edge)
 
-  (apply q/fill (:blue colors))
-  (q/rect 10 300 edge edge)
+  (apply q/fill (:blue (:colors state)))
+  (q/rect (first (:blue start-points)) (second (:blue start-points)) edge edge)
 
-  (apply q/fill (:green colors))
-  (q/rect 300 300 edge edge))
+  (apply q/fill (:green (:colors state)))
+  (q/rect (first (:green start-points)) (second (:green start-points)) edge edge))
 
 
 (defn button-clicked [state event]
   (log/info "CLICKED AT" event)
-  (log/info "Color: " (point->button (:button-points state)
-                                     (:x event)
-                                     (:y event)))
-  state)
+  (let [color (point->button (:button-points state)
+                             (:x event)
+                             (:y event))]
+    (log/info "Color: " color)
+    (if color
+      (assoc-in state [:colors color] (color secondary-colors))
+      state)))
 
 (q/defsketch simon
   :title "Simon says..."
@@ -92,7 +102,7 @@
   ; setup function called only once, during sketch initialization.
   :setup setup
   ; update-state is called on each iteration before draw-state.
-  ;:update update-state
+  :update update-state
   :draw draw-state
   :features [:keep-on-top :no-bind-output]
   :mouse-clicked button-clicked
